fd8c2dc8a3b70c3e53191c47814c6982cf58ecad
Daniel <230083@fppiramide.com>
2024-10-18 11:54:38 +0200

    fix estructura.sql

--- estructura.sql
+++ estructura.sql
@@ -1,22 +1,22 @@
 create database boleybol;
 use boleybol;
 
-create table equipos{
+create table equipos(
     id int auto_increment,
     nombre varchar(255),
     puntos int,
     constraint pkEquipos primary key (id)
-};
+);
 
-create table grupos{
+create table grupos(
     id int auto_increment,
     nombre varchar(255),
     idEquipo int,
     constraint pkGrupos primary key (id),
     constraint fkEquipoGrupo foreign key(idEquipo) references equipos(id)
-};
+);
 
-create table partidos{
+create table partidos(
     id int auto_increment,
     idEquipo1 int,
     idEquipo2 int,
@@ -27,9 +27,9 @@ create table partidos{
     constraint pkPartidos primary key (id),
     constraint fkEquipo1Partidos foreign key (idEquipo1) references grupos(id),
     constraint fkEquipo2Partidos foreign key (idEquipo2) references grupos(id)
-};
+);
 
-create table sets{
+create table sets(
     id int auto_increment,
     idPartido int,
     equipo1 int,
@@ -37,4 +37,4 @@ create table sets{
     numero int,
     constraint pkSets primary key(id),
     constraint fkPartidoSets foreign key(IdPartido)
-}
+)
