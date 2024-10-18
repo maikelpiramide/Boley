0c9192c66220e2cf59ba613d49986f78705c91e2
Maikel <230254@fppiramide.com>
2024-10-18 10:29:24 +0200

    UPDATE: fichero de BBDD añadido

---
+++ estructura.sql
@@ -0,0 +1,38 @@
+create database boleybol;
+use boleybol;
+
+create table equipos{
+    id int auto_increment,
+    nombre varchar(255),
+    puntos int,
+    constraint pkEquipos primary key (id)
+};
+
+create table grupos{
+    id int auto_increment,
+    nombre varchar(255),
+    idEquipo int,
+    constraint pkGrupos primary key (id),
+    constraint fkEquipoGrupo foreign key(idEquipo) references equipos(id)
+};
+
+create table partidos{
+    id int auto_increment,
+    idEquipo1 int,
+    idEquipo2 int,
+    p1 int,
+    p2 int,
+    constraint pkPartidos primary key (id),
+    constraint fkEquipo1Partidos foreign key (idEquipo1) references grupos(id),
+    constraint fkEquipo2Partidos foreign key (idEquipo2) references grupos(id),
+};
+
+create table sets{
+    id int auto_increment,
+    idPartido int,
+    equipo1 int,
+    equipo2 int,
+    numero int,
+    constraint pkSets primary key(id),
+    constraint fkPartidoSets foreign key(IdPartido)
+}
