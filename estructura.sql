e97e8f483dabfbad4fd4a5e9b10efab04d06d521
Maikel <230254@fppiramide.com>
2024-10-18 10:47:17 +0200

    UPDATE: BBDD fichero

--- estructura.sql
+++ estructura.sql
@@ -22,9 +22,11 @@ create table partidos{
     idEquipo2 int,
     p1 int,
     p2 int,
+    f1 int,
+    f2 int,
     constraint pkPartidos primary key (id),
     constraint fkEquipo1Partidos foreign key (idEquipo1) references grupos(id),
-    constraint fkEquipo2Partidos foreign key (idEquipo2) references grupos(id),
+    constraint fkEquipo2Partidos foreign key (idEquipo2) references grupos(id)
 };
 
 create table sets{
